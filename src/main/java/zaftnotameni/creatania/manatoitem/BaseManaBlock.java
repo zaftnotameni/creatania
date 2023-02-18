b3efe95cef5a765a390e6318e5f9dfdf4b40972c
zaftnotameni <mozair.carmo+zaft@gmail.com>
2023-02-18 21:29:28 +0100

    cull faces

--- src/main/java/zaftnotameni/creatania/manatoitem/BaseManaBlock.java
+++ src/main/java/zaftnotameni/creatania/manatoitem/BaseManaBlock.java
@@ -1,17 +1,30 @@
 package zaftnotameni.creatania.manatoitem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import zaftnotameni.creatania.util.Voxel;
 public abstract class BaseManaBlock  extends Block {
   public BaseManaBlock(Properties pProperties) {
     super(pProperties);
   }
 
   public static BlockBehaviour.Properties makeManablockProperties() {
-    return BlockBehaviour.Properties.of(Material.STONE)
+    return BlockBehaviour.Properties.copy(Blocks.GLASS)
       .destroyTime(1f)
       .lightLevel(x -> 15)
       .hasPostProcess((x, y, z) -> true)
-      .emissiveRendering((x, y, z) -> true);
+      .emissiveRendering((x, y, z) -> true)
+      .noOcclusion()
+      .isViewBlocking((x, y, z) -> false);
+  }
+
+  @Override
+  public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+    return Voxel.ALMOST_FULL_BLOCK_VOXEL;
   }
 }
