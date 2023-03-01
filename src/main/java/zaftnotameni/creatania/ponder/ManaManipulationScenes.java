c23522d2f7091265ac3b0bde3e74cf63ecab3c77
zaftnotameni <mozair.carmo+zaft@gmail.com>
2023-03-01 17:38:39 +0100

    auto formatting

--- src/main/java/zaftnotameni/creatania/ponder/ManaManipulationScenes.java
+++ src/main/java/zaftnotameni/creatania/ponder/ManaManipulationScenes.java
@@ -37,53 +37,93 @@ public class ManaManipulationScenes {
     var drain = x.at(0, 1, 1);
     setupInitialMixerScene(x, blaze, basin, mixer);
     addManaBlocksAsIngredients(x, blockId, blaze);
-    superheatBlazeBurner(x,  blaze);
-    processManaBlocks(x, fluidId,  basin, mixer);
-    drainFluidMana(x, fluidId,  basin, drain);
+    superheatBlazeBurner(x, blaze);
+    processManaBlocks(x, fluidId, basin, mixer);
+    drainFluidMana(x, fluidId, basin, drain);
     outputExplanationText(x, drain);
   }
 
   private static void outputExplanationText(CreataniaPonderUtils x, XYZ drain) {
-    x.keyFrameText("Generates Fluid Mana", drain.top(), 40).run();
+    x
+      .keyFrameText("Generates Fluid Mana", drain.top(), 40)
+      .run();
   }
 
   private static void setupInitialMixerScene(CreataniaPonderUtils x, XYZ blaze, XYZ basin, XYZ mixer) {
     x.scene.world.modifyBlock(blaze.asBlockPos(), x.unlit(), false);
-    x.idleAfter(20, x.scene::showBasePlate).get();
-    x.idleAfter(20, blaze.appearTo(EAST)).get();
-    x.idleAfter(20, basin.appearTo(WEST)).get();
-    x.idleAfter(20, mixer.appearTo(UP)).get();
+    x
+      .idleAfter(20, x.scene::showBasePlate)
+      .get();
+    x
+      .idleAfter(20, blaze.appearTo(EAST))
+      .get();
+    x
+      .idleAfter(20, basin.appearTo(WEST))
+      .get();
+    x
+      .idleAfter(20, mixer.appearTo(UP))
+      .get();
   }
 
-  private static void addManaBlocksAsIngredients( CreataniaPonderUtils x, ResourceLocation blockId,  XYZ basin) {
-    x.keyFrameText("Throw some mana blocks into the mix", basin.top(), 60).run();
-    x.showInputItem(blockId, Pointing.DOWN, basin.offsetTop(), 60).run();
+  private static void addManaBlocksAsIngredients(CreataniaPonderUtils x, ResourceLocation blockId, XYZ basin) {
+    x
+      .keyFrameText("Throw some mana blocks into the mix", basin.top(), 60)
+      .run();
+    x
+      .showInputItem(blockId, Pointing.DOWN, basin.offsetTop(), 60)
+      .run();
     x.scene.idle(60);
-    x.idleAfter(20, x.dropBlocksOnto(basin, blockId, 4, i -> x.scene.idle(20))).get();
+    x
+      .idleAfter(20, x.dropBlocksOnto(basin, blockId, 4, i -> x.scene.idle(20)))
+      .get();
   }
 
   private static void drainFluidMana(CreataniaPonderUtils x, ResourceLocation fluidId, XYZ basin, XYZ drain) {
-    drain.setBlock(AllBlocks.ITEM_DRAIN.getDefaultState()).run();
-    x.idleAfter(1,drain.appearTo(EAST)).get();
-    x.idleAfter(1,basin.modifyBlock(x.facing(BasinBlock.FACING, WEST))).get();
-    x.idleAfter(1,basin.modifyNbt(BasinTileEntity.class, x.emptyTanksFn("OutputTanks"))).get();
-    x.idleAfter(1,drain.modifyNbt(ItemDrainTileEntity.class, x.fillTanksFn("Tanks", fluidId, 1000))).get();
+    drain
+      .setBlock(AllBlocks.ITEM_DRAIN.getDefaultState())
+      .run();
+    x
+      .idleAfter(1, drain.appearTo(EAST))
+      .get();
+    x
+      .idleAfter(1, basin.modifyBlock(x.facing(BasinBlock.FACING, WEST)))
+      .get();
+    x
+      .idleAfter(1, basin.modifyNbt(BasinTileEntity.class, x.emptyTanksFn("OutputTanks")))
+      .get();
+    x
+      .idleAfter(1, drain.modifyNbt(ItemDrainTileEntity.class, x.fillTanksFn("Tanks", fluidId, 1000)))
+      .get();
   }
 
-  private static void processManaBlocks( CreataniaPonderUtils x, ResourceLocation fluidId, XYZ basin, XYZ mixer) {
-    mixer.modifyNbt( MechanicalMixerTileEntity.class, x.startProcessingFn()).run();
-    basin.modifyNbt( BasinTileEntity.class, x.fillTanksFn("OutputTanks", fluidId, 1000)).run();
+  private static void processManaBlocks(CreataniaPonderUtils x, ResourceLocation fluidId, XYZ basin, XYZ mixer) {
+    mixer
+      .modifyNbt(MechanicalMixerTileEntity.class, x.startProcessingFn())
+      .run();
+    basin
+      .modifyNbt(BasinTileEntity.class, x.fillTanksFn("OutputTanks", fluidId, 1000))
+      .run();
     x.scene.idle(10);
-    basin.modifyNbt( BasinTileEntity.class, x.emptyInputItemsFn()).run();
+    basin
+      .modifyNbt(BasinTileEntity.class, x.emptyInputItemsFn())
+      .run();
     x.scene.idle(100);
   }
 
-  private static void superheatBlazeBurner(  CreataniaPonderUtils x, XYZ blaze) {
-    x.idleAfter(20, blaze.modifyBlock( x.heated())).get();
-    x.keyFrameText("Superheat the blaze burner", blaze.center(), 30 ).run();
-    x.showInputItem(AllItems.BLAZE_CAKE.get(), Pointing.UP, blaze.center(), 30, InputWindowElement::rightClick).run();
+  private static void superheatBlazeBurner(CreataniaPonderUtils x, XYZ blaze) {
+    x
+      .idleAfter(20, blaze.modifyBlock(x.heated()))
+      .get();
+    x
+      .keyFrameText("Superheat the blaze burner", blaze.center(), 30)
+      .run();
+    x
+      .showInputItem(AllItems.BLAZE_CAKE.get(), Pointing.UP, blaze.center(), 30, InputWindowElement::rightClick)
+      .run();
     x.scene.idle(30);
-    x.idleAfter(40, blaze.modifyBlock( x.superHeated())).get();
+    x
+      .idleAfter(40, blaze.modifyBlock(x.superHeated()))
+      .get();
   }
 
 }
