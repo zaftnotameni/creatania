90030217395c0a7f5d3fcbc598d59e25be7fb093
zaftnotameni <mozair.carmo+zaft@gmail.com>
2023-03-01 17:28:56 +0100

    finetune timings for animation

--- src/main/java/zaftnotameni/creatania/ponder/CreataniaPonderUtils.java
+++ src/main/java/zaftnotameni/creatania/ponder/CreataniaPonderUtils.java
@@ -3,7 +3,12 @@ package zaftnotameni.creatania.ponder;
 import com.simibubi.create.content.contraptions.processing.burner.BlazeBurnerBlock;
 import com.simibubi.create.foundation.ponder.SceneBuilder;
 import com.simibubi.create.foundation.ponder.SceneBuildingUtil;
+import com.simibubi.create.foundation.ponder.Selection;
+import com.simibubi.create.foundation.ponder.element.InputWindowElement;
+import com.simibubi.create.foundation.utility.Pointing;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -11,24 +16,43 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+
+import static java.util.function.Function.identity;
+
 public class CreataniaPonderUtils {
+
   public Vec3 gentleFall() { return util.vector.of(0.0, -0.1, 0.0); }
 
-  public Vec3 offsetTopOf(BlockPos target) {
-    return util.vector.topOf(target).add(0.0, 0.5, 0.0);
+  public Runnable dropBlocksOnto(XYZ target, ResourceLocation blockId, final int howMany, Consumer<Integer> afterEach) {
+    return () -> {
+      if (howMany <= 0) return;
+      dropBlockOnto(target, blockId).run();
+      afterEach.accept(howMany);
+      dropBlocksOnto(target, blockId, howMany - 1, afterEach).run();
+    };
   }
-  public void dropBlocksOnto(BlockPos target, ResourceLocation blockId, int howMany, Consumer<Integer> afterEach) {
-    while(howMany-- > 0) { dropBlockOnto(target, blockId); afterEach.accept(howMany); }
+
+  public Runnable dropBlockOnto(XYZ target, ResourceLocation blockId) {
+    return () -> {
+      var stack = stackOfBlock(blockId);
+      scene.world.createItemEntity(target.offsetTop(), gentleFall(), stack);
+    };
   }
-  public void dropBlockOnto(BlockPos target,  ResourceLocation blockId) {
-    var stack = new ItemStack(ForgeRegistries.BLOCKS.getValue(blockId).asItem(), 1);
-    scene.world.createItemEntity(offsetTopOf(target), gentleFall(), stack);
+
+  @NotNull public static ItemStack stackOfBlock(ResourceLocation blockId) {
+    return new ItemStack(ForgeRegistries.BLOCKS
+                           .getValue(blockId)
+                           .asItem(), 1);
   }
+
   public Consumer<CompoundTag> emptyLevelFn() {
     return (CompoundTag nbt) -> {
       nbt.putFloat("Speed", 0.25f);
@@ -36,6 +60,7 @@ public class CreataniaPonderUtils {
       nbt.putFloat("Value", 1.0f);
     };
   }
+
   public Consumer<CompoundTag> fullLevelFn() {
     return (CompoundTag nbt) -> {
       nbt.putFloat("Speed", 0.25f);
@@ -43,12 +68,14 @@ public class CreataniaPonderUtils {
       nbt.putFloat("Value", 0.0f);
     };
   }
+
   public Consumer<CompoundTag> emptyTankContentFn() {
     return (CompoundTag nbt) -> {
       nbt.putInt("Amount", 0);
       nbt.putString("FluidName", "minecraft:empty");
     };
   }
+
   public Consumer<CompoundTag> emptyTankFn() {
     return (CompoundTag nbt) -> {
       var level = new CompoundTag();
@@ -59,13 +86,18 @@ public class CreataniaPonderUtils {
       nbt.put("TankContent", tank);
     };
   }
+
   public Consumer<CompoundTag> tankWithFluidContentFn(ResourceLocation fluidId, int amount) {
-    var fluid = ForgeRegistries.FLUIDS.getValue(fluidId).getRegistryName().toString();
+    var fluid = ForgeRegistries.FLUIDS
+      .getValue(fluidId)
+      .getRegistryName()
+      .toString();
     return (CompoundTag nbt) -> {
       nbt.putInt("Amount", amount);
       nbt.putString("FluidName", fluid);
     };
   }
+
   public Consumer<CompoundTag> tankWithAmountFn(ResourceLocation fluidId, int amount) {
     return (CompoundTag nbt) -> {
       var level = new CompoundTag();
@@ -76,11 +108,13 @@ public class CreataniaPonderUtils {
       nbt.put("TankContent", tank);
     };
   }
+
   public Consumer<CompoundTag> startProcessingFn() {
     return (CompoundTag nbt) -> {
       nbt.putBoolean("Running", true);
     };
   }
+
   public Consumer<CompoundTag> emptyInputItemsFn() {
     return (CompoundTag nbt) -> {
       var items = new CompoundTag();
@@ -89,6 +123,7 @@ public class CreataniaPonderUtils {
       nbt.put("InputItems", items);
     };
   }
+
   public Consumer<CompoundTag> emptyTanksFn(String tankPropertyName) {
     return (CompoundTag nbt) -> {
       var tanks = new ListTag();
@@ -101,6 +136,7 @@ public class CreataniaPonderUtils {
       nbt.put(tankPropertyName, tanks);
     };
   }
+
   public Consumer<CompoundTag> fillTanksFn(String tankPropertyName, ResourceLocation fluidId, int amount) {
     return (CompoundTag nbt) -> {
       var tanks = new ListTag();
@@ -113,26 +149,119 @@ public class CreataniaPonderUtils {
       nbt.put(tankPropertyName, tanks);
     };
   }
+
   public UnaryOperator<BlockState> unlit() {
     return (BlockState blockstate) -> blockstate.setValue(BlazeBurnerBlock.HEAT_LEVEL, BlazeBurnerBlock.HeatLevel.SMOULDERING);
   }
+
   public UnaryOperator<BlockState> heated() {
     return (BlockState blockstate) -> blockstate.setValue(BlazeBurnerBlock.HEAT_LEVEL, BlazeBurnerBlock.HeatLevel.KINDLED);
   }
+
   public UnaryOperator<BlockState> superHeated() {
     return (BlockState blockstate) -> blockstate.setValue(BlazeBurnerBlock.HEAT_LEVEL, BlazeBurnerBlock.HeatLevel.SEETHING);
   }
+
   public UnaryOperator<BlockState> facing(Direction direction) {
     return facing(BlockStateProperties.FACING, direction);
   }
+
   public UnaryOperator<BlockState> facing(DirectionProperty property, Direction direction) {
     return (BlockState blockstate) -> blockstate.setValue(property, direction);
   }
+
+  public Supplier<Integer> idleAfter(int ticks, Consumer<Integer> action) {
+    return () -> {
+      action.accept(ticks);
+      scene.idle(ticks);
+      return ticks;
+    };
+  }
+
+  public Supplier<Integer> idleBefore(int ticks, Consumer<Integer> action) {
+    return () -> {
+      scene.idle(ticks);
+      action.accept(ticks);
+      return ticks;
+    };
+  }
+
+  public Supplier<Integer> idleAfter(int ticks, Runnable action) { return idleAfter(ticks, i -> action.run()); }
+
+  public Supplier<Integer> idleBefore(int ticks, Runnable action) { return idleBefore(ticks, i -> action.run()); }
+
   public SceneBuilder scene;
   public SceneBuildingUtil util;
+
   public CreataniaPonderUtils(SceneBuilder scene, SceneBuildingUtil util) {
     this.scene = scene;
     this.util = util;
   }
+
   public static CreataniaPonderUtils of(SceneBuilder scene, SceneBuildingUtil util) { return new CreataniaPonderUtils(scene, util); }
+
+  public Runnable keyFrameText(String text, Vec3 target, int duration) {
+    return () -> scene.overlay
+      .showText(duration)
+      .attachKeyFrame()
+      .text(text)
+      .pointAt(target);
+  }
+
+  public XYZ at(int x, int y, int z) { return new XYZ(this, x, y, z); }
+
+  public Runnable showInputItem(ResourceLocation blockId, Pointing pointing, Vec3 target, int duration) {
+    var item = ForgeRegistries.BLOCKS.getValue(blockId).asItem();
+    return showInputItem(item, pointing, target, duration, identity());
+  }
+  public Runnable showInputItem(ItemLike item, Pointing pointing, Vec3 target, int duration) {
+    return showInputItem(item, pointing, target, duration, identity());
+  }
+  public Runnable showInputItem(ItemLike item, Pointing pointing, Vec3 target, int duration, Function<InputWindowElement, InputWindowElement> fn) {
+    return () -> {
+      var popover = new InputWindowElement(target, pointing)
+        .withItem(new ItemStack(item, 1));
+      fn.apply(popover);
+      scene.overlay.showControls(popover, duration);
+    };
+  }
+
+  public static class XYZ {
+
+    public Vec3 asVec3() { return new Vec3(x, y, z); }
+
+    public BlockPos asBlockPos() { return u.util.grid.at(x, y, z); }
+
+    public Vec3 center() { return u.util.vector.centerOf(asBlockPos()); }
+
+    public Vec3 top() { return u.util.vector.topOf(asBlockPos()); }
+
+    public Vec3 offsetTop(double offset) { return top().add(0.0, offset, 0.0); }
+
+    public Vec3 offsetTop() { return offsetTop(0.5); }
+
+    public Runnable appearTo(Direction direction) { return () -> u.scene.world.showSection(asSelection(), direction); }
+
+    public Runnable appearFrom(Direction direction) { return appearTo(direction.getOpposite()); }
+
+    public Runnable setBlock(BlockState blockState) { return () -> u.scene.world.setBlock(asBlockPos(), blockState, false); }
+    public Runnable modifyBlock(UnaryOperator<BlockState> blockstateFunc) { return () -> u.scene.world.modifyBlock(asBlockPos(), blockstateFunc, false); }
+    public Runnable modifyNbt(Class<? extends BlockEntity> type, Consumer<CompoundTag> nbtFn) { return () -> u.scene.world.modifyTileNBT(asSelection(), type, nbtFn, true); }
+
+    public Selection asSelection() { return u.util.select.position(x, y, z); }
+
+    public CreataniaPonderUtils u;
+    public int x;
+    public int y;
+    public int z;
+
+    public XYZ(CreataniaPonderUtils u, int x, int y, int z) {
+      this.u = u;
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
+
+  }
+
 }
--- src/main/java/zaftnotameni/creatania/ponder/ManaManipulationScenes.java
+++ src/main/java/zaftnotameni/creatania/ponder/ManaManipulationScenes.java
@@ -1,4 +1,5 @@
 package zaftnotameni.creatania.ponder;
+
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllItems;
 import com.simibubi.create.content.contraptions.components.mixer.MechanicalMixerTileEntity;
@@ -7,112 +8,82 @@ import com.simibubi.create.content.contraptions.processing.BasinBlock;
 import com.simibubi.create.content.contraptions.processing.BasinTileEntity;
 import com.simibubi.create.foundation.ponder.SceneBuilder;
 import com.simibubi.create.foundation.ponder.SceneBuildingUtil;
-import com.simibubi.create.foundation.ponder.Selection;
 import com.simibubi.create.foundation.ponder.element.InputWindowElement;
 import com.simibubi.create.foundation.utility.Pointing;
-import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
+import zaftnotameni.creatania.ponder.CreataniaPonderUtils.XYZ;
 import zaftnotameni.creatania.registry.Blocks;
 import zaftnotameni.creatania.registry.Fluids;
 
 import static net.minecraft.core.Direction.*;
+
 public class ManaManipulationScenes {
+
   public static void makeLiquidPureMana(SceneBuilder scene, SceneBuildingUtil util) {
     makeManaFluidFromBlocksScene(scene, util, Blocks.PURE_MANA_BLOCK.getId(), Fluids.PURE_MANA.getId());
   }
+
   public static void makeLiquidCorruptMana(SceneBuilder scene, SceneBuildingUtil util) {
     makeManaFluidFromBlocksScene(scene, util, Blocks.CORRUPT_MANA_BLOCK.getId(), Fluids.CORRUPT_MANA.getId());
   }
+
   public static void makeManaFluidFromBlocksScene(SceneBuilder scene, SceneBuildingUtil util, ResourceLocation blockId, ResourceLocation fluidId) {
-    scene.title("mana_" + blockId.getPath() + "_to_" + fluidId.getPath(), "Making Mana Fluid");
-    scene.scaleSceneView(1.5f);
     var x = new CreataniaPonderUtils(scene, util);
-    var basinPos = util.grid.at(1, 2, 1);
-    var mixerPos = util.grid.at(1, 4, 1);
-    var blazePos = util.grid.at(1, 1, 1);
-    var drainPos = util.grid.at(0, 1, 1);
-    var basin = util.select.position(basinPos);
-    var mixer = util.select.position(mixerPos);
-    var blaze = util.select.position(blazePos);
-    var drain = util.select.position(drainPos);
-    setupInitialMixerScene(scene, x, blazePos, basin, mixer, blaze);
-    addManaBlocksAsIngredients(scene, util, blockId, x, basinPos);
-    superheatBlazeBurner(scene, util, x, blazePos);
-    processManaBlocks(scene, fluidId, x, basin, mixer);
-    drainFluidMana(scene, fluidId, x, basinPos, drainPos, basin, drain);
-    outputExplanationText(scene, util, drainPos);
+    x.scene.title("mana_" + blockId.getPath() + "_to_" + fluidId.getPath(), "Making Mana Fluid");
+    x.scene.scaleSceneView(1.5f);
+    var basin = x.at(1, 2, 1);
+    var mixer = x.at(1, 4, 1);
+    var blaze = x.at(1, 1, 1);
+    var drain = x.at(0, 1, 1);
+    setupInitialMixerScene(x, blaze, basin, mixer);
+    addManaBlocksAsIngredients(x, blockId, blaze);
+    superheatBlazeBurner(x,  blaze);
+    processManaBlocks(x, fluidId,  basin, mixer);
+    drainFluidMana(x, fluidId,  basin, drain);
+    outputExplanationText(x, drain);
   }
-  private static void outputExplanationText(SceneBuilder scene, SceneBuildingUtil util, BlockPos drainPos) {
-    scene.overlay
-      .showText(40)
-      .attachKeyFrame()
-      .text("Generates fluid mana")
-      .pointAt(util.vector.topOf(drainPos));
-  }
-  private static void setupInitialMixerScene(SceneBuilder scene, CreataniaPonderUtils x, BlockPos blazePos, Selection basin, Selection mixer, Selection blaze) {
-    scene.world.modifyBlock(blazePos, x.unlit(), false);
-    scene.showBasePlate();
-    scene.idle(20);
-    scene.world.showSection(blaze, EAST);
-    scene.idle(20);
-    scene.world.showSection(basin, WEST);
-    scene.idle(20);
-    scene.world.showSection(mixer, UP);
-    scene.idle(20);
-  }
-  private static void addManaBlocksAsIngredients(SceneBuilder scene, SceneBuildingUtil util, ResourceLocation blockId, CreataniaPonderUtils x, BlockPos basinPos) {
-    var stack = new ItemStack(ForgeRegistries.BLOCKS.getValue(blockId).asItem(), 1);
-    scene.overlay
-      .showText(60)
-      .attachKeyFrame()
-      .text("Throw some mana blocks into the mix")
-      .pointAt(util.vector.centerOf(basinPos));
-    scene.overlay
-      .showControls(
-      new InputWindowElement(util.vector.topOf(basinPos), Pointing.DOWN).withItem(stack),
-      60);
-    scene.idle(60);
 
-    x.dropBlocksOnto(basinPos, blockId, 4, i -> scene.idle(20));
-    scene.idle(20);
+  private static void outputExplanationText(CreataniaPonderUtils x, XYZ drain) {
+    x.keyFrameText("Generates Fluid Mana", drain.top(), 40).run();
   }
-  private static void drainFluidMana(SceneBuilder scene, ResourceLocation fluidId, CreataniaPonderUtils x, BlockPos basinPos, BlockPos drainPos, Selection basin, Selection drain) {
-    scene.world.setBlock(drainPos, AllBlocks.ITEM_DRAIN.getDefaultState(), false);
-    scene.world.showSection(drain, EAST);
-    scene.idle(1);
 
-    scene.world.modifyBlock(basinPos, x.facing(BasinBlock.FACING, WEST), false);
-    scene.idle(1);
+  private static void setupInitialMixerScene(CreataniaPonderUtils x, XYZ blaze, XYZ basin, XYZ mixer) {
+    x.scene.world.modifyBlock(blaze.asBlockPos(), x.unlit(), false);
+    x.idleAfter(20, x.scene::showBasePlate).get();
+    x.idleAfter(20, blaze.appearTo(EAST)).get();
+    x.idleAfter(20, basin.appearTo(WEST)).get();
+    x.idleAfter(20, mixer.appearTo(UP)).get();
+  }
 
-    scene.world.modifyTileNBT(basin, BasinTileEntity.class, x.emptyTanksFn("OutputTanks"));
-    scene.idle(1);
+  private static void addManaBlocksAsIngredients( CreataniaPonderUtils x, ResourceLocation blockId,  XYZ basin) {
+    x.keyFrameText("Throw some mana blocks into the mix", basin.top(), 60).run();
+    x.showInputItem(blockId, Pointing.DOWN, basin.offsetTop(), 60).run();
+    x.scene.idle(60);
+    x.idleAfter(20, x.dropBlocksOnto(basin, blockId, 4, i -> x.scene.idle(20))).get();
+  }
 
-    scene.world.modifyTileNBT(drain, ItemDrainTileEntity.class, x.fillTanksFn("Tanks", fluidId, 1000));
-    scene.idle(1);
+  private static void drainFluidMana(CreataniaPonderUtils x, ResourceLocation fluidId, XYZ basin, XYZ drain) {
+    drain.setBlock(AllBlocks.ITEM_DRAIN.getDefaultState()).run();
+    x.idleAfter(1,drain.appearTo(EAST)).get();
+    x.idleAfter(1,basin.modifyBlock(x.facing(BasinBlock.FACING, WEST))).get();
+    x.idleAfter(1,basin.modifyNbt(BasinTileEntity.class, x.emptyTanksFn("OutputTanks"))).get();
+    x.idleAfter(1,drain.modifyNbt(ItemDrainTileEntity.class, x.fillTanksFn("Tanks", fluidId, 1000))).get();
   }
-  private static void processManaBlocks(SceneBuilder scene, ResourceLocation fluidId, CreataniaPonderUtils x, Selection basin, Selection mixer) {
-    scene.world.modifyTileNBT(mixer, MechanicalMixerTileEntity.class, x.startProcessingFn());
-    scene.world.modifyTileNBT(basin, BasinTileEntity.class, x.emptyInputItemsFn());
-    scene.world.modifyTileNBT(basin, BasinTileEntity.class, x.fillTanksFn("OutputTanks", fluidId, 1000));
-    scene.idle(20);
+
+  private static void processManaBlocks( CreataniaPonderUtils x, ResourceLocation fluidId, XYZ basin, XYZ mixer) {
+    mixer.modifyNbt( MechanicalMixerTileEntity.class, x.startProcessingFn()).run();
+    basin.modifyNbt( BasinTileEntity.class, x.fillTanksFn("OutputTanks", fluidId, 1000)).run();
+    x.scene.idle(10);
+    basin.modifyNbt( BasinTileEntity.class, x.emptyInputItemsFn()).run();
+    x.scene.idle(100);
   }
-  private static void superheatBlazeBurner(SceneBuilder scene, SceneBuildingUtil util, CreataniaPonderUtils x, BlockPos blazePos) {
-    scene.world.modifyBlock(blazePos, x.heated(), false);
-    scene.idle(20);
-    scene.overlay
-      .showText(30)
-      .attachKeyFrame()
-      .text("Superheat your blaze burner")
-      .pointAt(util.vector.centerOf(blazePos));
-    scene.overlay
-      .showControls(
-        new InputWindowElement(util.vector.centerOf(blazePos), Pointing.UP)
-          .withItem(new ItemStack(AllItems.BLAZE_CAKE.get(), 1))
-          .rightClick(),
-        30);
-    scene.world.modifyBlock(blazePos, x.superHeated(), false);
-    scene.idle(40);
+
+  private static void superheatBlazeBurner(  CreataniaPonderUtils x, XYZ blaze) {
+    x.idleAfter(20, blaze.modifyBlock( x.heated())).get();
+    x.keyFrameText("Superheat the blaze burner", blaze.center(), 30 ).run();
+    x.showInputItem(AllItems.BLAZE_CAKE.get(), Pointing.UP, blaze.center(), 30, InputWindowElement::rightClick).run();
+    x.scene.idle(30);
+    x.idleAfter(40, blaze.modifyBlock( x.superHeated())).get();
   }
+
 }
