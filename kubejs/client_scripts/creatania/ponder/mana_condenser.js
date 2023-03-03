a12558d983f1a1b600849ec228100e47c01f8c0f
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-03-02 19:34:57 -0700

    Update mana_condenser.js
    Added key frames
    Added some more corrupt blocks to belt

--- kubejs/client_scripts/creatania/ponder/mana_condenser.js
+++ kubejs/client_scripts/creatania/ponder/mana_condenser.js
@@ -19,13 +19,13 @@ const displayManaCondenserFromBlocksScene = (params) => (scene, util) => {
 
   scene.world.showSection(shaft, Facing.EAST);
   scene.idle(20);
-  
+
   scene.text(80, "Providing rotational force to Mana Condensers produces Corrupt Inert Mana Blocks", offsetCenterOf(util)(shaft));
   scene.idle(90);
-  
-    scene.world.showSection(barrel, Facing.NORTH);
-    scene.idle(20);
-  
+
+  scene.world.showSection(barrel, Facing.NORTH);
+  scene.idle(20);
+
   scene.text(80, "Place Mana Condenser over any inventory with space to collect mana blocks", offsetCenterOf(util)(barrel));
 
 };
@@ -41,7 +41,8 @@ const displayManaCondenserFromBlocksScene2 = (params) => (scene, util) => {
   const shaft = util.grid.at(1, 2, 2);
   const barrel = util.grid.at(5, 2, 2);
   const funnel = util.grid.at(4, 2, 2);
-  const mana_block = util.grid.at(4, 1, 2);
+  // const mana_block = util.grid.at(4, 1, 2);
+  const outputCondenser = util.grid.at(2, 1, 2);
 
   // fix the mana condenser cardinal facing direction
   scene.world.modifyBlock(condenser, blockstate => blockstate.with("facing", "west"), false);
@@ -58,14 +59,29 @@ const displayManaCondenserFromBlocksScene2 = (params) => (scene, util) => {
 
   scene.world.showSection(barrel, Facing.NORTH);
   scene.world.showSection(funnel, Facing.NORTH);
-  scene.idle(20);
+  scene.idle(10);
 
+  scene.addKeyframe();
   scene.text(80, "Mana Condensers can also be placed over Mechanical Belts", offsetCenterOf(util)(belt));
-  scene.idle(20);
-
+  
   // removed mana block from belt
   scene.world.removeItemsFromBelt(belt_shaft2);
+  scene.idle(10);
+
+
+  // add a few more mana blocks
+  for (let index = 0; index < 20; index++) {
+    if (index < 20) {
+      scene.world.createItemOnBeltLike(outputCondenser, Direction.UP, "creatania:mana/blocks/corrupt");
+      scene.idle(10);
+      // removed mana block from belt
+      scene.world.removeItemsFromBelt(belt_shaft2);
+    }
+  }
+
+  scene.addKeyframe();
   scene.idle(20);
+  scene.text(120, "If the inventory or belt is full, then the Mana Condensers will stop sending out blocks.", offsetCenterOf(util)(barrel));
 
 };
 
