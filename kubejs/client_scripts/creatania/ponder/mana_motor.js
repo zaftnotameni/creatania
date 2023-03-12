947d1e7410bfcacf84e90729dba189c9f3984177
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-03-11 17:26:39 -0700

    Most done. Might tweak later

--- kubejs/client_scripts/creatania/ponder/mana_motor.js
+++ kubejs/client_scripts/creatania/ponder/mana_motor.js
@@ -8,18 +8,32 @@ const displayManaMotorFromBlocksScene = (params) => (scene, util) => {
   const manapool = util.grid.at(0, 1, 1);
   const spark_1 = util.grid.at(3, 2, 1);
   const spark_2 = util.grid.at(0, 2, 1);
-  // const agument = util.grid.at(3, 2, 1);
 
   scene.showBasePlate();
   scene.idle(20);
+  scene.text(60, "The mysterious Mana Motor is a puzzle, but somehow runs on Mana", offsetTopOf(util)(motor));
   scene.world.showSection(motor, Facing.NORTH);
   scene.world.showSection(manapool, Facing.NORTH);
+  scene.idle(70);
+
+  scene.addKeyframe();
+  scene.text(70, "Add a Spark over the Mana Motor - unfortunately sparks are shy and won't let us show them", offsetTopOf(util)(spark_1));
+  scene.idle(80);
   scene.world.showSection(spark_1, Facing.NORTH);
+
+  scene.text(40, "And a Spark plus a Recessive Augment over the Mana Pool", offsetTopOf(util)(spark_2));
+  scene.idle(50);
   scene.world.showSection(spark_2, Facing.NORTH);
-  // scene.world.showSection(agument, Facing.NORTH);
   scene.addKeyframe();
   scene.idle(20);
 
+  scene.text(70, "The Mana Motor speed is controlled with your mouse wheel over the center of this panel", offsetCenterOf(util)(motor));
+
+
+
+  /* 
+  /setblock -79 -58 -37 creatania:mana_motor[axis_along_first=true,facing=north]{Network:{AddedCapacity:1.0f,Capacity:64.0f,Id:-21440476889146L,Size:2,Stress:0.0f},ScrollValue:64,Speed:-64.0f,active:1b,duct:0b,mana:99992,manaPerTick:8}
+  */
 
 };
 
