db73b7746518b58e95a44827d4a71f588322ea52
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-03-11 17:26:58 -0700

    Final for beta

--- kubejs/client_scripts/creatania/ponder/blazunia.js
+++ kubejs/client_scripts/creatania/ponder/blazunia.js
@@ -1,5 +1,15 @@
 // Ponder scenes for purifying corrupt mana to pure mana
 
+/* 
+/setblock -51 -58 -27 create:blaze_burner[blaze=smouldering,facing=east]{burnTimeRemaining:0,fuelLevel:0}
+
+
+/setblock -49 -56 -31 create:blaze_burner[blaze=seething,facing=south]{burnTimeRemaining:2800,fuelLevel:2}
+
+
+
+*/
+
 const displayBlazuniaFromBlocksScene = (params) => (scene, util) => {
 
   // const blockId = params.blockId;
@@ -31,7 +41,30 @@ const displayBlazuniaFromBlocksScene = (params) => (scene, util) => {
 
   scene.showBasePlate();
   scene.text(60, "What if a Mana functional flower could power Blaze Burners?");
-  scene.idle(71);
+  scene.idle(70);
+
+  /* blazeState = (block, state, facing)
+    @param block variable: 
+    @param state string: "smouldering", "kindled", "seething"
+    @param state string: "north", "south", "east", "west", "up", "down"
+  */
+  const blazeState = (block, state, facing) => {
+    scene.world.modifyBlock(block, (blockstate) => {
+      return blockstate.with("blaze", state).with("facing", facing);
+    }, false);
+    // console.log("Item: " + block);
+    // console.log("State: " + state);
+    // console.log("Facing: " + facing);
+  };
+
+  // blazeState(blaze_1, "smouldering", "west");
+
+
+  const arrBLAZE_BURNERS = [blaze_1, blaze_2, blaze_3, blaze_4, blaze_5, blaze_6, blaze_7, blaze_8, blaze_9];
+
+  arrBLAZE_BURNERS.forEach(block => {
+    blazeState(block, "smouldering", "west");
+  });
 
   scene.world.showSection(blaze_1, Facing.DOWN);
   scene.world.showSection(blaze_2, Facing.DOWN);
@@ -42,7 +75,7 @@ const displayBlazuniaFromBlocksScene = (params) => (scene, util) => {
   scene.world.showSection(blaze_7, Facing.DOWN);
   scene.world.showSection(blaze_8, Facing.DOWN);
   scene.world.showSection(blaze_9, Facing.DOWN);
-  scene.idle(21);
+  scene.idle(20);
 
   scene.world.showSection(tank_1, Facing.DOWN);
   scene.world.showSection(tank_2, Facing.DOWN);
@@ -53,56 +86,51 @@ const displayBlazuniaFromBlocksScene = (params) => (scene, util) => {
   scene.world.showSection(tank_7, Facing.DOWN);
   scene.world.showSection(tank_8, Facing.DOWN);
   scene.world.showSection(tank_9, Facing.DOWN);
-  scene.idle(21);
-  
-  
-  scene.text(80, "Let's meet the Blaznia. It's a firey one!", offsetCenterOf(util)(blazunia));  
+  scene.idle(20);
+
+
+  scene.text(80, "Let's meet the Blaznia. It's a firey one!", offsetCenterOf(util)(blazunia));
   scene.world.setBlock(blazunia, "creatania:blazunia", false);
   scene.world.showSection(blazunia, Facing.EAST);
-  scene.idle(71);
-  
-  scene.world.showSection(manapool, Facing.WEST);
-  scene.idle(21);
+  scene.idle(70);
 
+  scene.addKeyframe();
+  scene.text(40, "Just add Mana", offsetCenterOf(util)(manapool));
+  scene.world.showSection(manapool, Facing.WEST);
+  scene.idle(10);
+  
+  // ignite blazunia
   scene.world.modifyBlock(blazunia, (blockstate) => {
     return blockstate.with("has_mana_source", true).with("is_superhot", false);
   }, false);
+
+  // display ignited blazunia
   scene.world.modifyTileNBT(blazunia, (nbt) => {
     nbt.floating = false;
     nbt.mana = 80;
     nbt.pool = { x: 2, y: 1, z: 1 };
   });
+  
+  // blaze burners changed to kindled
+  arrBLAZE_BURNERS.forEach(block => {
+    blazeState(block, "kindled", "west");
+  });
+  scene.text(60, "Burn baby burn ... watch them burn!");
+  scene.idle(110);
 
-  // The blaze burners changed to kindled
-  const blazes_kindled = [1, 2, 3, 4, 7];
-  for (const index in blazes_kindled) {
-    let theburner =  "blaze_" + blazes_kindled[index];
-    console.log(theburner);
-    scene.world.modifyBlock(theburner, (blockstate) => {
-        return blockstate.with("blaze", "kindled").with("facing", "west");
-      }, false);
-  };
-  scene.world.modifyBlock(blaze_1, (blockstate) => {
-    return blockstate.with("blaze", "kindled").with("facing", "west");
-  }, false);
-  scene.world.modifyBlock(blaze_2, (blockstate) => {
-    return blockstate.with("blaze", "kindled").with("facing", "west");
-  }, false);
-  scene.world.modifyBlock(blaze_3, (blockstate) => {
-    return blockstate.with("blaze", "kindled").with("facing", "west");
-  }, false);
-  scene.world.modifyBlock(blaze_4, (blockstate) => {
-    return blockstate.with("blaze", "kindled").with("facing", "west");
-  }, false);
-  scene.world.modifyBlock(blaze_7, (blockstate) => {
-    return blockstate.with("blaze", "kindled").with("facing", "west");
-  }, false);
-  scene.world.showSection(pylon_1, Facing.EAST);
-  scene.world.showSection(pylon_2, Facing.EAST);
-  scene.world.showSection(pylon_3, Facing.EAST);
-  scene.world.showSection(pylon_4, Facing.EAST);
-  scene.idle(21);
-
+  scene.addKeyframe();
+  scene.text(60, "Super Heated Wrath of Hades!");
+  scene.world.showSection(pylon_1, Facing.UP);
+  scene.world.showSection(pylon_2, Facing.UP);
+  scene.world.showSection(pylon_3, Facing.UP);
+  scene.world.showSection(pylon_4, Facing.UP);
+  scene.idle(10);
+  
+  // blaze burners changed to seething
+  arrBLAZE_BURNERS.forEach(block => {
+    blazeState(block, "seething", "west");
+  });
+  scene.idle(60);
 
 };
 
